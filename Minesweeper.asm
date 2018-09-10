73a738f8c1fbff5cb7beab1df3826136a88d8f2f
Cole Cummins <colejcummins@gmail.com>
2018-09-09 20:06:04 -0700

    Update Minesweeper.asm

--- Minesweeper.asm
+++ Minesweeper.asm
@@ -53,12 +53,12 @@ board: .DB 0x40, 0x01, 0x00, 0x01, 0x40, 0x01, 0x02, 0x40
 
 
 init:		MOV R3, 0x0A
-			CALL draw_init
-			SEI
+		CALL draw_init
+		SEI
 
 
 input:		IN  R17, 0x44
-			BRN input
+		BRN input
 
 
 ;MAIN=================================
@@ -72,93 +72,93 @@ input:		IN  R17, 0x44
 ;checks the keypress and branches to 
 ;a different method based on the key
 ISR:		CMP	R17, 0x1D
-			BREQ move_up
-			CMP R17, 0x1C
-			BREQ move_left
-			CMP R17, 0x1B
-			BREQ move_down
-			CMP R17, 0x23
-			BREQ move_right
-			CMP R17, 0x2D
-			BREQ mov_reveal
-			CMP R17, 0x2B
-			BREQ move_flag
-			RETIE
+		BREQ move_up
+		CMP R17, 0x1C
+		BREQ move_left
+		CMP R17, 0x1B
+		BREQ move_down
+		CMP R17, 0x23
+		BREQ move_right
+		CMP R17, 0x2D
+		BREQ mov_reveal
+		CMP R17, 0x2B
+		BREQ move_flag
+		RETIE
 
 ;checks to see if cursor is in
 ;bounds for a key press up
 move_up:	CMP R16, 0x00
-			BRNE move_up_cn
-			RETIE
+		BRNE move_up_cn
+		RETIE
 
 ;moves the cursor up
 move_up_cn:	SUB R16, 0x01
-			CALL draw_cursr
-			RETIE
+		CALL draw_cursr
+		RETIE
 
 ;checks if the cursor is in
 ;bounds for a key press left
 move_left:	CMP R15, 0x00
-			BRNE move_lf_cn
-			RETIE
+		BRNE move_lf_cn
+		RETIE
 
 ;moves the cursor left
 move_lf_cn: SUB R15, 0x01
-			CALL draw_cursr
-			RETIE
+		CALL draw_cursr
+		RETIE
 
 ;checks if the cursor is in
 ;bounds for a key press down
 move_down:	CMP R16, 0x07
-			BRNE move_dn_cn
-			RETIE
+		BRNE move_dn_cn
+		RETIE
 
 ;moves the cursor down
 move_dn_cn: ADD R16, 0x01
-			CALL draw_cursr
-			RETIE
+		CALL draw_cursr
+		RETIE
 
 ;checks if the cursor is in 
 ;bounds for a key press right
-move_right: CMP R15, 0x07
-			BRNE move_rt_cn
-			RETIE
+move_right: 	CMP R15, 0x07
+		BRNE move_rt_cn
+		RETIE
 
 ;moves the cursor right
-move_rt_cn: ADD R15, 0x01
-			CALL draw_cursr
-			RETIE
+move_rt_cn: 	ADD R15, 0x01
+		CALL draw_cursr
+		RETIE
 
 ;reveals the box at cursor 
 ;location, if the box has
 ;a mine, sets the game lost
-mov_reveal: MOV R23, R15
-			MOV R24, R16
-			CALL reveal_box
-			CMP R2, 0x01
-			BREQ game_lost
-			CALL upd_board
-			RETIE
+mov_reveal: 	MOV R23, R15
+		MOV R24, R16
+		CALL reveal_box
+		CMP R2, 0x01
+		BREQ game_lost
+		CALL upd_board
+		RETIE
 
 ;flags the box at cursor 
 ;location, if all mines
 ;are flagged, sets the game
 ;won
 move_flag:	MOV R23, R15
-			MOV R24, R16
-			CALL flag_box
-			CALL upd_board
-			CMP	R1, 0x01
-			BREQ game_won
-			RETIE
+		MOV R24, R16
+		CALL flag_box
+		CALL upd_board
+		CMP R1, 0x01
+		BREQ game_won
+		RETIE
 
 ;game won state, draws a smile!
 game_won:	CALL draw_smile
-			BRN game_won
+		BRN game_won
 
 ;game lost state, draws a frown :(
 game_lost:	CALL draw_frown
-			BRN game_lost
+		BRN game_lost
 ;END MAIN=============================
 
 
@@ -171,36 +171,36 @@ game_lost:	CALL draw_frown
 
 ;loads box from mem and checks to see
 ;if box has already been revealed
-reveal_box: CALL x_y_to_mem	
-			LD  R26, (R25)
-			CALL check_revl
-			CMP R29, 0x80
-			BRNE rv_box_flg
-			RET 
+reveal_box: 	CALL x_y_to_mem	
+		LD  R26, (R25)
+		CALL check_revl
+		CMP R29, 0x80
+		BRNE rv_box_flg
+		RET 
 
 ;check to see if box is flagged, if so
 ;return from the reveal subroutine 
-rv_box_flg: CALL check_flag
-			CMP R31, 0x20
-			BRNE rv_box_min
-			RET
+rv_box_flg: 	CALL check_flag
+		CMP R31, 0x20
+		BRNE rv_box_min
+		RET
 
 ;checks to see if mine is revealed, if
 ;so set the game as lost
 rv_box_min:	CALL check_mine
-			CMP R30, 0x40
-			BRNE rv_box_blk
-			MOV R2, 0x01
-			RET
+		CMP R30, 0x40
+		BRNE rv_box_blk
+		MOV R2, 0x01
+		RET
 
 ;reveals box, checks adjacent, if zero
 ;recursively reveals adjacent mines
-rv_box_blk: OR	R26, 0x80
-			ST  R26, (R25)
-			CALL check_adjc
-			CMP R28, 0x00
-			BREQ rv_box_all
-			RET
+rv_box_blk: 	OR R26, 0x80
+		ST  R26, (R25)
+		CALL check_adjc
+		CMP R28, 0x00
+		BREQ rv_box_all
+		RET
 
 
 ;RECURSIVE REVEAL======================
@@ -212,111 +212,111 @@ rv_box_blk: OR	R26, 0x80
 ;stack when exiting the subroutine
 ;======================================
 
-rv_box_all: CALL rv_bx_left
-			CALL rev_box_up
-			CALL rv_box_rit		
-			CALL rv_box_bot	
-			RET
+rv_box_all: 	CALL rv_bx_left
+		CALL rev_box_up
+		CALL rv_box_rit		
+		CALL rv_box_bot	
+		RET
 
-rv_bx_left: CMP R23, 0x00
-			BRNE rv_bx_lf_l
-			RET
+rv_bx_left: 	CMP R23, 0x00
+		BRNE rv_bx_lf_l
+		RET
 
 ;reveal left most box
-rv_bx_lf_l: PUSH R23
-			PUSH R24
-			SUB R23, 0x01
-			CALL reveal_box
-			POP R24
-			POP R23
-			CMP R24, 0x00
-			BRNE rv_bx_lf_u
-			RET
+rv_bx_lf_l: 	PUSH R23
+		PUSH R24
+		SUB R23, 0x01
+		CALL reveal_box
+		POP R24
+		POP R23
+		CMP R24, 0x00
+		BRNE rv_bx_lf_u
+		RET
 
 ;reveal top left box
-rv_bx_lf_u: PUSH R23
-			PUSH R24
-			SUB R23, 0x01
-			SUB R24, 0x01
-			CALL reveal_box
-			POP R24
-			POP R23
-			RET
+rv_bx_lf_u: 	PUSH R23
+		PUSH R24
+		SUB R23, 0x01
+		SUB R24, 0x01
+		CALL reveal_box
+		POP R24
+		POP R23
+		RET
 
-rev_box_up: CMP R24, 0x00
-			BRNE rv_bx_up_u
-			RET
+rev_box_up: 	CMP R24, 0x00
+		BRNE rv_bx_up_u
+		RET
 
 ;reveal top most box
 rv_bx_up_u:	PUSH R23
-			PUSH R24
-			SUB R24, 0x01
-			CALL reveal_box
-			POP R24
-			POP R23
-			CMP R23, 0x07
-			BRNE rv_bx_up_r
-			RET
+		PUSH R24
+		SUB R24, 0x01
+		CALL reveal_box
+		POP R24
+		POP R23
+		CMP R23, 0x07
+		BRNE rv_bx_up_r
+		RET
 
 ;reveal top right box
-rv_bx_up_r: PUSH R23
-			PUSH R24
-			ADD R23, 0x01
-			SUB R24, 0x01
-			CALL reveal_box
-			POP R24
-			POP R23
-			RET
+rv_bx_up_r: 	PUSH R23
+		PUSH R24
+		ADD R23, 0x01
+		SUB R24, 0x01
+		CALL reveal_box
+		POP R24
+		POP R23
+		RET
 
-rv_box_rit: CMP R23, 0x07
-			BRNE rv_bx_rt_r
-			RET
+rv_box_rit: 	CMP R23, 0x07
+		BRNE rv_bx_rt_r
+		RET
 
 ;reveal right most box
-rv_bx_rt_r: PUSH R23
-			PUSH R24
-			ADD R23, 0x01
-			CALL reveal_box 
-			POP R24
-			POP R23
-			CMP R24, 0x07
-			BRNE rv_bx_rt_b
-			RET
+rv_bx_rt_r: 	PUSH R23
+		PUSH R24
+		ADD R23, 0x01
+		CALL reveal_box 
+		POP R24
+		POP R23
+		CMP R24, 0x07
+		BRNE rv_bx_rt_b
+		RET
 
 ;reveal bottom right box
-rv_bx_rt_b: PUSH R23
-			PUSH R24
-			ADD R23, 0x01
-			ADD R24, 0x01
-			CALL reveal_box
-			POP R24
-			POP R23
-			RET
+rv_bx_rt_b: 	PUSH R23
+		PUSH R24
+		ADD R23, 0x01
+		ADD R24, 0x01
+		CALL reveal_box
+		POP R24
+		POP R23
+		RET
 
-rv_box_bot: CMP R24, 0x07
-			BRNE rv_bx_bt_b
-			RET
+rv_box_bot: 	CMP R24, 0x07
+		BRNE rv_bx_bt_b
+		RET
 
 ;reveal bottom most box
-rv_bx_bt_b: PUSH R23
-			PUSH R24
-			ADD R24, 0x01
-			CALL reveal_box
-			POP R24
-			POP R23
-			CMP R23, 0x00
-			BRNE rv_bx_bt_l
-			RET
+rv_bx_bt_b: 	PUSH R23
+		PUSH R24
+		ADD R24, 0x01
+		CALL reveal_box
+		POP R24
+		POP R23
+		CMP R23, 0x00
+		BRNE rv_bx_bt_l
+		RET
 
 ;reveal bottom left box
-rv_bx_bt_l: PUSH R23
-			PUSH R24
-			SUB R23, 0x01
-			ADD R24, 0x01
-			CALL reveal_box
-			POP R24
-			POP R23
-			RET
+rv_bx_bt_l: 	PUSH R23
+		PUSH R24
+		SUB R23, 0x01
+		ADD R24, 0x01
+		CALL reveal_box
+		POP R24
+		POP R23
+		RET
 ;END REVEAL===========================
 
 
@@ -330,44 +330,44 @@ rv_bx_bt_l: PUSH R23
 ;load box from mem, if box has not been
 ;revealed, flag or unflag
 flag_box:	CALL x_y_to_mem
-			LD	R26, (R25)
-			CALL check_revl
-			CMP R29, 0x80
-			BRNE flag_con
-			RET
+		LD R26, (R25)
+		CALL check_revl
+		CMP R29, 0x80
+		BRNE flag_con
+		RET
 
 ;check to see if box has flag, if so 
 ;remove flag, checks to see if box has
 ;mine, if so decrement unflagged mines
 flag_con:	CALL check_flag
-			CMP R31, 0x20
-			BREQ remove_flg
-			OR 	R26, 0x20
-			CALL check_mine
-			CMP	R30, 0x40
-			BREQ add_flag
-			BRN flag_done
+		CMP R31, 0x20
+		BREQ remove_flg
+		OR R26, 0x20
+		CALL check_mine
+		CMP R30, 0x40
+		BREQ add_flag
+		BRN flag_done
 
 ;if box has mine, decrement unflagged mines,
 ;checks to see if game is won
 add_flag:	SUB R3, 0x01
-			CMP R3, 0x00
-			BRNE flag_done
-			ADD R1, 0x01
-			RET
+		CMP R3, 0x00
+		BRNE flag_done
+		ADD R1, 0x01
+		RET
 
 ;removes flag if square is already flagged,
 ;increases unflagged mine count if box had
 ;a mine
 remove_flg:	AND R26, 0xDF
-			CALL check_mine
-			CMP R30, 0x40
-			BRNE flag_done
-			ADD R3, 0x01
-			BRN flag_done
+		CALL check_mine
+		CMP R30, 0x40
+		BRNE flag_done
+		ADD R3, 0x01
+		BRN flag_done
 
-flag_done:	ST	R26, (R25)
-			RET
+flag_done:	ST R26, (R25)
+		RET
 ;END FLAG=============================
 
 
@@ -380,336 +380,336 @@ flag_done:	ST	R26, (R25)
 ;initializes the board, draws the background
 ;lines in between squares, and face
 draw_init:	CALL draw_background
-			CALL drw_hz_lns
-			CALL drw_vt_lns
-			CALL draw_face
-			CALL draw_cursr
-			RET
+		CALL drw_hz_lns
+		CALL drw_vt_lns
+		CALL draw_face
+		CALL draw_cursr
+		RET
 
 ;updates the board, drawing each individual
 ;square as well as the cursor
 upd_board:	MOV R8, 0x01
-			MOV R7, 0x00
-			MOV R25, 0x00
-			BRN upd_brd_lp
+		MOV R7, 0x00
+		MOV R25, 0x00
+		BRN upd_brd_lp
 
 ;loops through the board, loading a box 
 ;from memory then drawing it		
-upd_brd_lp: LD R26, (R25)
-			CALL mem_to_x_y
-			CALL draw_loc
-			CALL check_flag
-			CALL check_adjc
-			CALL check_revl 
-			CMP R29, 0x80
-			BREQ upd_brd_rv
-			CMP R31, 0x20
-			BREQ upd_b_flag
-			MOV R6, 0x92
-			CALL clear_sqr
-			BRN upd_brd_fn
+upd_brd_lp: 	LD R26, (R25)
+		CALL mem_to_x_y
+		CALL draw_loc
+		CALL check_flag
+		CALL check_adjc
+		CALL check_revl 
+		CMP R29, 0x80
+		BREQ upd_brd_rv
+		CMP R31, 0x20
+		BREQ upd_b_flag
+		MOV R6, 0x92
+		CALL clear_sqr
+		BRN upd_brd_fn
 
 ;draws a square that is revealed, decides
 ;the value to be drawn based on its 
 ;adjacency
-upd_brd_rv: CMP R28, 0x00
-			BREQ upd_b_blnk
-			CMP R28, 0x01
-			BREQ upd_b_one
-			CMP R28, 0x02
-			BREQ upd_b_two
-			CMP R28, 0x03
-			BREQ upd_b_thre
-			CMP R28, 0x04
-			BREQ upd_b_four
-			BRN upd_brd_fn
+upd_brd_rv: 	CMP R28, 0x00
+		BREQ upd_b_blnk
+		CMP R28, 0x01
+		BREQ upd_b_one
+		CMP R28, 0x02
+		BREQ upd_b_two
+		CMP R28, 0x03
+		BREQ upd_b_thre
+		CMP R28, 0x04
+		BREQ upd_b_four
+		BRN upd_brd_fn
 
 ;clears a square with no adjacency			
-upd_b_blnk: MOV R6, 0x73
-			CALL clear_sqr
-			BRN upd_brd_fn
+upd_b_blnk: 	MOV R6, 0x73
+		CALL clear_sqr
+		BRN upd_brd_fn
 
 ;draws a "one"
 upd_b_one: 	CALL draw_one
-			BRN upd_brd_fn
+		BRN upd_brd_fn
 
 ;draws a "two"
 upd_b_two:	CALL draw_two
-			BRN upd_brd_fn
+		BRN upd_brd_fn
 
 ;draws a "three"
 upd_b_thre:	CALL draw_three
-			BRN upd_brd_fn
+		BRN upd_brd_fn
 
 ;draws a "four"
 upd_b_four:	CALL draw_four
-			BRN upd_brd_fn
+		BRN upd_brd_fn
 
 ;draws a "flag"
 upd_b_flag:	CALL draw_flag
-			BRN upd_brd_fn
+		BRN upd_brd_fn
 
 ;finalizes the loop and increments memory	
 upd_brd_fn:	ADD R25, 0x01
-			CMP R25, 0x40
-			BRNE upd_brd_lp
-			RET
+		CMP R25, 0x40
+		BRNE upd_brd_lp
+		RET
 
 ;clears a given square on, drawing in a given
 ;color
 clear_sqr: 	MOV R9, R8
-			ADD R9, 0x02
-			CALL draw_horizontal_line
-			ADD R7, 0x01
-			CALL draw_horizontal_line
-			CMP R7, 0x1D
-			BRNE fn_clr_sqr
-			RET
+		ADD R9, 0x02
+		CALL draw_horizontal_line
+		ADD R7, 0x01
+		CALL draw_horizontal_line
+		CMP R7, 0x1D
+		BRNE fn_clr_sqr
+		RET
 
 ;finishes clear square method
-fn_clr_sqr: ADD R7, 0x01
-			CALL draw_horizontal_line
-			RET
+fn_clr_sqr: 	ADD R7, 0x01
+		CALL draw_horizontal_line
+		RET
 
 ;Draws the cursor, checks to see if cursor
 ;is in bounds of the board, redraws the 
 ;horizontal and vertical lines to clear
 ;the previous cursor
-draw_cursr: CALL drw_hz_lns
-			CALL drw_vt_lns
-			MOV R23, R15
-			MOV R24, R16
-			CALL draw_loc
-			MOV R6, 0xF4
-			SUB R8, 0x01
-			CALL draw_dot
-			ADD R8, 0x04
-			CALL draw_dot
-			SUB R8, 0x03
-			CMP R16, 0x00
-			BRNE draw_c_top
-			BRN con_drw_cr
+draw_cursr: 	CALL drw_hz_lns
+		CALL drw_vt_lns
+		MOV R23, R15
+		MOV R24, R16
+		CALL draw_loc
+		MOV R6, 0xF4
+		SUB R8, 0x01
+		CALL draw_dot
+		ADD R8, 0x04
+		CALL draw_dot
+		SUB R8, 0x03
+		CMP R16, 0x00
+		BRNE draw_c_top
+		BRN con_drw_cr
 
-con_drw_cr: CMP R16, 0x1C
-			BRNE draw_c_bot
-			RET
+con_drw_cr: 	CMP R16, 0x1C
+		BRNE draw_c_bot
+		RET
 
 ;draws the top part of the cursor
 draw_c_top:	SUB R8, 0x01
-			SUB R7, 0x01
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			ADD R8, 0x02
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot	
-			SUB	R8, 0x03
-			ADD R7, 0x01
-			BRN con_drw_cr
+		SUB R7, 0x01
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		ADD R8, 0x02
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot	
+		SUB R8, 0x03
+		ADD R7, 0x01
+		BRN con_drw_cr
 
 ;draws the bottom part of the cursor
-draw_c_bot: ADD R7, 0x02
-			SUB R8, 0x01
-			CALL draw_dot
-			ADD R7, 0x01
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			ADD R8, 0x02
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			SUB R7 ,0x01
-			CALL draw_dot
-			RET
+draw_c_bot: 	ADD R7, 0x02
+		SUB R8, 0x01
+		CALL draw_dot
+		ADD R7, 0x01
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		ADD R8, 0x02
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		SUB R7 ,0x01
+		CALL draw_dot
+		RET
 
 ;draws the horizontal lines
 ;on the board
-drw_hz_lns: MOV R6, 0x00
-			MOV R7, 0x03
-			MOV R9, 0x1F
-			BRN con_drw_hz
+drw_hz_lns: 	MOV R6, 0x00
+		MOV R7, 0x03
+		MOV R9, 0x1F
+		BRN con_drw_hz
 
-con_drw_hz: MOV R8, 0x00
-			CALL draw_horizontal_line
-			CMP R7, 0x1B
-			BREQ fn_draw_hz
-			ADD R7, 0x04
-			BRN con_drw_hz
+con_drw_hz: 	MOV R8, 0x00
+		CALL draw_horizontal_line
+		CMP R7, 0x1B
+		BREQ fn_draw_hz
+		ADD R7, 0x04
+		BRN con_drw_hz
 
-fn_draw_hz: RET
+fn_draw_hz: 	RET
 	
 ;draws the vertical lines
 ;on the board
-drw_vt_lns: MOV R6, 0x00
-			MOV R8, 0x00
-			MOV R9, 0x1D
-			BRN con_drw_vt
+drw_vt_lns: 	MOV R6, 0x00
+		MOV R8, 0x00
+		MOV R9, 0x1D
+		BRN con_drw_vt
 
-con_drw_vt: MOV R7, 0x00
-			CALL draw_vertical_line
-			CMP R8, 0x20
-			BREQ fn_draw_vt
-			ADD R8, 0x04
-			BRN con_drw_vt
+con_drw_vt: 	MOV R7, 0x00
+		CALL draw_vertical_line
+		CMP R8, 0x20
+		BREQ fn_draw_vt
+		ADD R8, 0x04
+		BRN con_drw_vt
 
-fn_draw_vt: RET
+fn_draw_vt: 	RET
 
 ;draws the default face on the side
 ;of the screen
-draw_face:  MOV R6, 0xF4
-			MOV R7, 0x01
-			MOV R8, 0x23
-			CALL draw_dot
-			ADD R7, 0x01	
-			CALL draw_dot
-			ADD R8, 0x02
-			CALL draw_dot
-			SUB R7, 0x01
-			CALL draw_dot
-			MOV R7, 0x05
-			MOV R8, 0x22
-			MOV R9, 0x26	
-			CALL draw_horizontal_line
-			MOV R8, 0x00
-			MOV R7, 0x00
-			RET
+draw_face:  	MOV R6, 0xF4
+		MOV R7, 0x01
+		MOV R8, 0x23
+		CALL draw_dot
+		ADD R7, 0x01	
+		CALL draw_dot
+		ADD R8, 0x02
+		CALL draw_dot
+		SUB R7, 0x01
+		CALL draw_dot
+		MOV R7, 0x05
+		MOV R8, 0x22
+		MOV R9, 0x26	
+		CALL draw_horizontal_line
+		MOV R8, 0x00
+		MOV R7, 0x00
+		RET
 
 ;draws a smile if game is won
-draw_smile: MOV R6, 0xF4
-			MOV R7, 0x04
-			MOV R8, 0x22
-			CALL draw_dot
-			MOV R8, 0x26
-			CALL draw_dot
-			RET
+draw_smile: 	MOV R6, 0xF4
+		MOV R7, 0x04
+		MOV R8, 0x22
+		CALL draw_dot
+		MOV R8, 0x26
+		CALL draw_dot
+		RET
 
 ;draws a frown if game is lost
-draw_frown: MOV R6, 0xF4
-			MOV R7, 0x06
-			MOV R8, 0x22
-			CALL draw_dot	
-			MOV R8, 0x26
-			CALL draw_dot
-			RET
+draw_frown: 	MOV R6, 0xF4
+		MOV R7, 0x06
+		MOV R8, 0x22
+		CALL draw_dot	
+		MOV R8, 0x26
+		CALL draw_dot
+		RET
 
 ;draws the number one
 draw_one:	MOV R6, 0x03
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			ADD R7, 0x01
-			CALL draw_dot
-			CMP R7, 0x1D
-			BRNE f_draw_one
-			SUB R7, 0x01
-			SUB R8, 0x01
-			RET
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		ADD R7, 0x01
+		CALL draw_dot
+		CMP R7, 0x1D
+		BRNE f_draw_one
+		SUB R7, 0x01
+		SUB R8, 0x01
+		RET
 
 ;continues if in bounds
 f_draw_one:	ADD R7, 0x01
-			CALL draw_dot
-			SUB R8, 0x01
-			CALL draw_dot
-			ADD R8, 0x02
-			CALL draw_dot
-			CALL reset_x_y
-			RET
+		CALL draw_dot
+		SUB R8, 0x01
+		CALL draw_dot
+		ADD R8, 0x02
+		CALL draw_dot
+		CALL reset_x_y
+		RET
 
 ;draws the number two
 draw_two: 	MOV R6, 0x14
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			ADD R7, 0x01
-			CALL draw_dot
-			CMP R7, 0x1D
-			BRNE f_draw_two
-			SUB R7, 0x01
-			SUB R8, 0x01
-			RET
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		ADD R7, 0x01
+		CALL draw_dot
+		CMP R7, 0x1D
+		BRNE f_draw_two
+		SUB R7, 0x01
+		SUB R8, 0x01
+		RET
 
 ;continues if in bounds		
-f_draw_two: ADD R7, 0x01
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			CALL reset_x_y
-			RET
+f_draw_two: 	ADD R7, 0x01
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		CALL reset_x_y
+		RET
 
 ;draws the number three		
 draw_three:	MOV R6, 0x80
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			ADD R7, 0x01
-			CALL draw_dot
-			SUB R8, 0x01
-			CALL draw_dot
-			CMP R7, 0x1D
-			BRNE f_drw_thre
-			SUB R7, 0x01
-			SUB R8, 0x01
-			RET
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		ADD R7, 0x01
+		CALL draw_dot
+		SUB R8, 0x01
+		CALL draw_dot
+		CMP R7, 0x1D
+		BRNE f_drw_thre
+		SUB R7, 0x01
+		SUB R8, 0x01
+		RET
 
 ;continues if in bounds
-f_drw_thre: ADD R7, 0x01
-			CALL draw_dot
-			SUB R8, 0x01
-			CALL draw_dot
-			ADD R8, 0x02
-			CALL draw_dot
-			CALL reset_x_y
-			RET
+f_drw_thre: 	ADD R7, 0x01
+		CALL draw_dot
+		SUB R8, 0x01
+		CALL draw_dot
+		ADD R8, 0x02
+		CALL draw_dot
+		CALL reset_x_y
+		RET
 
 ;draws the number four
-draw_four:	MOV	R6, 0x82
-			CALL draw_dot
-			ADD R7, 0x01
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			SUB R7, 0x01
-			CALL draw_dot
-			CMP R7, 0x1C
-			BRNE f_drw_four
-			SUB R8, 0x02
-			RET
+draw_four:	MOV R6, 0x82
+		CALL draw_dot
+		ADD R7, 0x01
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		SUB R7, 0x01
+		CALL draw_dot
+		CMP R7, 0x1C
+		BRNE f_drw_four
+		SUB R8, 0x02
+		RET
 
 ;continues if in bounds
-f_drw_four: ADD R7, 0x02
-			CALL draw_dot
-			RET
+f_drw_four: 	ADD R7, 0x02
+		CALL draw_dot
+		RET
 
 ;draws a flagged space
 draw_flag: 	MOV R6, 0xE0
-			CALL draw_dot
-			ADD R7, 0x01
-			CALL draw_dot
-			ADD R8, 0x01
-			CALL draw_dot
-			SUB R7, 0x01
-			CALL draw_dot
-			MOV R6, 0xFF
-			ADD R8, 0x01
-			CALL draw_dot
-			ADD R7, 0x01
-			CALL draw_dot
-			CMP R7, 0x1D
-			BRNE f_drw_flag
-			SUB R7, 0x01
-			SUB R8, 0x02
-			RET
+		CALL draw_dot
+		ADD R7, 0x01
+		CALL draw_dot
+		ADD R8, 0x01
+		CALL draw_dot
+		SUB R7, 0x01
+		CALL draw_dot
+		MOV R6, 0xFF
+		ADD R8, 0x01
+		CALL draw_dot
+		ADD R7, 0x01
+		CALL draw_dot
+		CMP R7, 0x1D
+		BRNE f_drw_flag
+		SUB R7, 0x01
+		SUB R8, 0x02
+		RET
 
 ;continues if in bounds
 f_drw_flag:	ADD R7, 0x01
-			CALL draw_dot
-			RET	
+		CALL draw_dot
+		RET	
 
 ;draws a horizontal line to a set
 ;address across the screen
